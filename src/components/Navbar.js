2585ab0a66ab23b310a266fa7f936c378b3cd3e8
Andrew Bwogi <andrew.bwogi@gmail.com>
2022-10-22 18:59:09 -0700

    tailwind css gradients explained

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -15,7 +15,10 @@ function classNames(...classes) {
 
 export default function Navbar() {
 	return (
-		<Disclosure as='nav' className='bg-gray-800'>
+		<Disclosure
+			as='nav'
+			className='bg-gradient-to-r from-indigo-800 via-brown-300 to-gray-500 ...'
+		>
 			{({ open }) => (
 				<>
 					<div className='mx-auto max-w-7xl px-2 sm:px-6 lg:px-8'>
